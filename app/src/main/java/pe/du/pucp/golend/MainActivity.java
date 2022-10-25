88dc19e8f421576e270487153fd558e1c1db70b3
Angel <a20190192@pucp.edu.pe>
2022-10-25 13:14:27 -0500

    SplashScreen v2 e inputs v1

--- app/src/main/java/pe/du/pucp/golend/MainActivity.java
+++ app/src/main/java/pe/du/pucp/golend/MainActivity.java
@@ -1,16 +1,27 @@
 package pe.du.pucp.golend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import pe.du.pucp.golend.Anonymus.OnboardingActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser  == null || currentUser.isAnonymous()){
+            Intent intentAnonymus = new Intent(MainActivity.this, OnboardingActivity.class);
+            startActivity(intentAnonymus);
+            finish();
+        }
     }
 }
