075ccfbc171119a59cf8a9e722b3d539c99f5e55
Angel <a20190192@pucp.edu.pe>
2022-10-24 23:09:58 -0500

    splash screen v1

--- app/src/main/java/pe/du/pucp/golend/MainActivity.java
+++ app/src/main/java/pe/du/pucp/golend/MainActivity.java
@@ -1,6 +1,7 @@
 package pe.du.pucp.golend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
