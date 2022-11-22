59634e0dc2aa75e407303fbb43e29dcc7af1a6f9
Alessandra Morales <azmoralesm@pucp.edu.pe>
2022-11-22 18:33:32 -0500

    cosillas

---
+++ app/src/main/java/pe/du/pucp/golend/Cliente/ClienteReservasActivity.java
@@ -0,0 +1,16 @@
+package pe.du.pucp.golend.Cliente;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.du.pucp.golend.R;
+
+public class ClienteReservasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_reservas);
+    }
+}
