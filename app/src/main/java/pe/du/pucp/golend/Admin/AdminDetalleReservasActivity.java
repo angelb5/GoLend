5259497e9d88a20e7e993486d8ed91b03c6625af
Alessandra Morales <azmoralesm@pucp.edu.pe>
2022-11-22 21:05:27 -0500

    Detalle de reserva Admin - vista

---
+++ app/src/main/java/pe/du/pucp/golend/Admin/AdminDetalleReservasActivity.java
@@ -0,0 +1,16 @@
+package pe.du.pucp.golend.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.du.pucp.golend.R;
+
+public class AdminDetalleReservasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_detalle_reservas);
+    }
+}
