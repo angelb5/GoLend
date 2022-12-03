0ed65c49b0c0e9390a71d939445f9ba8c071312b
Angel <a20190192@pucp.edu.pe>
2022-12-03 14:30:57 -0500

    Perfil Administrador

--- app/src/main/java/pe/du/pucp/golend/Admin/AdminProfileActivity.java
+++ app/src/main/java/pe/du/pucp/golend/Admin/AdminProfileActivity.java
@@ -1,5 +1,7 @@
 package pe.du.pucp.golend.Admin;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -7,8 +9,8 @@ import androidx.core.app.ActivityCompat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -17,16 +19,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.gson.Gson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import pe.du.pucp.golend.Anonymus.LoginActivity;
+import pe.du.pucp.golend.ChooseAvatarActivity;
 import pe.du.pucp.golend.Entity.User;
 import pe.du.pucp.golend.R;
 
@@ -34,10 +41,11 @@ public class AdminProfileActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     EditText etUpdateNombre;
     EditText etUpdateCorreo;
-    TextView tvcodigo;
+    TextView tvCodigo;
+    TextView tvNombre;
     String  userName;
     String userCorreo;
-    CollectionReference user;
+    CollectionReference userRef;
     ImageView ivPfp;
     User userAdmin;
     SharedPreferences sharedPreferences;
@@ -49,14 +57,16 @@ public class AdminProfileActivity extends AppCompatActivity {
         setBottomNavigationView();
         etUpdateNombre = findViewById(R.id.etUpdateNombre);
         etUpdateCorreo = findViewById(R.id.etUpdateCorreo);
-        tvcodigo = findViewById(R.id.tvcodigo);
+        tvCodigo = findViewById(R.id.tvAdminProfileCodigo);
+        tvNombre = findViewById(R.id.tvAdminProfileNombre);
         ivPfp = findViewById(R.id.ivAdminProfilePfp);
-        user = FirebaseFirestore.getInstance().collection("users");
+        userRef = FirebaseFirestore.getInstance().collection("users");
 
         sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
         Gson gson = new Gson();
         userAdmin = gson.fromJson(sharedPreferences.getString("user",""),User.class);
-        tvcodigo.setText(userAdmin.getCodigo());
+        tvCodigo.setText(userAdmin.getCodigo());
+        tvNombre.setText(userAdmin.getNombre());
 
         userName= userAdmin.getNombre();
         userCorreo = FirebaseAuth.getInstance().getCurrentUser().getEmail();
@@ -102,18 +112,84 @@ public class AdminProfileActivity extends AppCompatActivity {
         finish();
     }
 
-    public void actualizarPerfilAdmin(AuthResult authResult, User user){
-        UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder()
-                .setDisplayName(String.valueOf(etUpdateNombre.getText())).build();
+    public void actualizarPerfilAdmin(View view){
 
-        authResult.getUser().updateProfile(userProfileChangeRequest);
+        String newNombre = etUpdateNombre.getText().toString().trim();
+        String newCorreo = etUpdateCorreo.getText().toString().trim();
+
+        boolean isInvalid = false;
+
+        if(newNombre.isEmpty()){
+            etUpdateNombre.setError("No puede estar vacío");
+            etUpdateNombre.requestFocus();
+            isInvalid = true;
+        }
+
+        if(newNombre.length()>30){
+            etUpdateNombre.setError("No puede tener más de 30 caracteres");
+            etUpdateNombre.requestFocus();
+            isInvalid = true;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(newCorreo).matches() || !(newCorreo.endsWith("pucp.edu.pe") || newCorreo.endsWith("pucp.pe"))){
+            etUpdateCorreo.setError("Ingrese un correo válido");
+            etUpdateCorreo.requestFocus();
+            isInvalid = true;
+        }
+        Map<String, Object> updates = new HashMap<>();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (isInvalid || firebaseUser == null) return;
+
+        if (!userAdmin.getNombre().equals(newNombre)) {
+            updates.put("nombre", newNombre);
+            tvNombre.setText(newNombre);
+            UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(String.valueOf(etUpdateNombre.getText())).build();
+            firebaseUser.updateProfile(userProfileChangeRequest);
+        }
+        if(!userAdmin.getCorreo().equals(newCorreo)) {
+            updates.put("correo", newCorreo);
+            firebaseUser.updateEmail(newCorreo);
+        }
+
+        userRef.document(firebaseUser.getUid()).update(updates).addOnSuccessListener(unused -> {
+            Gson gson = new Gson();
+            userAdmin.setNombre(newNombre);
+            userAdmin.setCorreo(newCorreo);
+            sharedPreferences.edit().putString("user", gson.toJson(userAdmin)).apply();
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AdminProfileActivity.this, "Hubo un error al actualizar los datos", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public void cerrarSesionAdmin(View view){
+    public void goToChooseAvatar(View view) {
+        Intent caIntent = new Intent(AdminProfileActivity.this, ChooseAvatarActivity.class);
+        caIntent.putExtra("avatarUrl", userAdmin.getAvatarUrl());
+        launcherChooseAvatar.launch(caIntent);
+    }
+
+    public void cerrarSesionAdmin(View view) {
         FirebaseAuth.getInstance().signOut();
         Toast.makeText(AdminProfileActivity.this, "Has cerrado sesión", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(AdminProfileActivity.this, LoginActivity.class));
         ActivityCompat.finishAffinity(AdminProfileActivity.this);
         finish();
     }
+
+    ActivityResultLauncher<Intent> launcherChooseAvatar = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+        result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                Intent data = result.getData();
+                if (data == null) return;
+                Gson gson = new Gson();
+                userAdmin.setAvatarUrl(data.getStringExtra("avatarUrl"));
+                Glide.with(AdminProfileActivity.this).load(userAdmin.getAvatarUrl()).into(ivPfp);
+                sharedPreferences.edit().putString("user", gson.toJson(userAdmin)).apply();
+            }
+        }
+    );
 }
